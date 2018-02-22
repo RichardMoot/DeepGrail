ce5a666d86d8fbc15ff5e07866c848d16effbf39
Richard Moot <richard.moot@icloud.com>
2018-02-22 17:03:56 +0100

    Moved many auxiliary functions to grail_data_utils.py

--- grail_data_utils.py
+++ grail_data_utils.py
@@ -349,6 +349,82 @@ def read_vecs(file):
     return words_to_index, index_to_words, word_to_vec_map
 
 
+def lists_to_indices(X, item_to_index, max_len, normalize=False):
+
+    m = X.shape[0]                                   # number of training examples
+    
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m,max_len))
+
+    for i in range(m):                               # loop over training examples
+        
+        # Convert the ith training sentence in lower case and split it into words. You should get a list of words.
+        list = X[i]
+
+        j = 0
+        
+        # Loop over the words of sentence_words
+        for w in list:
+            if normalize == True:
+                w = normalize_word(w)
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+            try:
+                X_indices[i, j] = item_to_index[w]
+            except:
+                print("Unknown: ", w)
+                X_indices[i, j] = 1  # unknown
+            # Increment j to j + 1
+            j = j + 1
+            
+    return X_indices
+
+# GRADED FUNCTION: sentences_to_indices
+
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()` (described in Figure 4). 
+    
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this. 
+    
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
+    
+    m = X.shape[0]                                   # number of training examples
+    
+    ### START CODE HERE ###
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m,max_len))
+    
+    for i in range(m):                               # loop over training examples
+        
+        # Convert the ith training sentence in lower case and split it into words. You should get a list of words.
+        sentence_words = X[i]
+        
+        # Initialize j to 0
+        j = 0
+        
+        # Loop over the words of sentence_words
+        for w in sentence_words:
+            w = normalize_word(w)
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+            try:
+                X_indices[i, j] = word_to_index[w]
+            except:
+                print("Unknown: ", w)
+                X_indices[i, j] = 1   # index for unknown words
+            # Increment j to j + 1
+            j = j + 1
+            
+    ### END CODE HERE ###
+    
+    return X_indices
+
+
 def plot_confusion_matrix(y_actu, y_pred, title='Confusion matrix', cmap=plt.cm.gray_r):
     
     df_confusion = pd.crosstab(y_actu, y_pred.reshape(y_pred.shape[0],), rownames=['Actual'], colnames=['Predicted'], margins=True)
