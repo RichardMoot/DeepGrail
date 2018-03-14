f282f1ac1891bfabbf068da8c6218e4268b29f4a
Richard Moot <richard.moot@icloud.com>
2018-03-14 16:02:01 +0100

    Added save_all() function

--- deeppos.py
+++ deeppos.py
@@ -30,6 +30,38 @@ def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
+def save_all():
+    save_obj(word_to_index, word_to_index)
+    save_obj(index_to_word, index_to_word)
+    save_obj(pos1_to_index, pos1_to_index)
+    save_obj(index_to_pos1, index_to_pos1)
+    save_obj(pos2_to_index, pos2_to_index)
+    save_obj(index_to_pos2, index_to_pos2)
+    save_obj(word_to_vec_map, word_to_vec_map)
+    save_obj(p1_to_integer, p1_to_integer)
+    save_obj(integer_to_p1, integer_to_p1)
+    save_obj(p2_to_integer, p2_to_integer)
+    save_obj(integer_to_21, integer_to_p2)
+    save_obj(p3_to_integer, p3_to_integer)
+    save_obj(integer_to_31, integer_to_p3)
+    save_obj(p4_to_integer, p4_to_integer)
+    save_obj(integer_to_p4, integer_to_p4)
+    save_obj(s1_to_integer, s1_to_integer)
+    save_obj(integer_to_s1, integer_to_s1)
+    save_obj(s2_to_integer, s2_to_integer)
+    save_obj(integer_to_s2, integer_to_s2)
+    save_obj(s3_to_integer, s3_to_integer)
+    save_obj(integer_to_s3, integer_to_s3)
+    save_obj(s4_to_integer, s4_to_integer)
+    save_obj(integer_to_s4, integer_to_s4)
+    save_obj(s5_to_integer, s5_to_integer)
+    save_obj(integer_to_s5, integer_to_s5)
+    save_obj(s6_to_integer, s6_to_integer)
+    save_obj(integer_to_s6, integer_to_s6)
+    save_obj(s7_to_integer, s7_to_integer)
+    save_obj(integer_to_s7, integer_to_s7)
+
+    
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
         return text[len(prefix):]
