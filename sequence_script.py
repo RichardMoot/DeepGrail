98bdb55c39f58468ab8f67bfe7b2be5dc3a207bf
Richard Moot <richard.moot@icloud.com>
2019-06-20 22:50:09 +0200

    Streamlined, added callbacks

--- sequence_script.py
+++ sequence_script.py
@@ -14,10 +14,14 @@ from my_classes import DataGenerator
 np.random.seed(1)
 
 params = {'n_pos1_classes':30,
-          'n_pos2_classes':31,
+          'n_pos2_classes':32,
           'n_super_classes':891,
           'shuffle':True,
           'batch_size':32}
+
+best_file = 'best_gen_elmo_superpos.h5'
+current_file = 'current_gen_elmo_superpos.h5'
+
 
 all = ["sent%06d" %i for i in range(15748)]
 train, testdev = train_test_split(all, test_size=0.4)
@@ -32,7 +36,7 @@ validation_generator = DataGenerator(dev, **params)
 
 embLen = 1024
 numPos1Classes = 30
-numPos2Classes = 31
+numPos2Classes = 32
 numSuperClasses = 891
 
 sentence_embeddings = Input(shape = (None,embLen,), dtype = 'float32')
@@ -49,13 +53,13 @@ X = Dropout(0.2)(X)
 
 Pos1 = TimeDistributed(Dense(32,kernel_constraint=max_norm(5.)))(X)
 Pos1 = TimeDistributed(Dropout(0.2))(Pos1)
-Pos1 = TimeDistributed(Dense(numPos1Classes, name='pos1', activation='softmax',kernel_constraint=max_norm(5.)))(Pos1)
+pos1_output = TimeDistributed(Dense(numPos1Classes, name='pos1_output', activation='softmax',kernel_constraint=max_norm(5.)))(Pos1)
 
 # Pos2 output
 
 Pos2 = TimeDistributed(Dense(32,kernel_constraint=max_norm(5.)))(X)
 Pos2 = TimeDistributed(Dropout(0.2))(Pos2)
-Pos2 = TimeDistributed(Dense(numPos2Classes, name='pos2', activation='softmax',kernel_constraint=max_norm(5.)))(Pos2)
+pos2_output = TimeDistributed(Dense(numPos2Classes, name='pos2_output', activation='softmax',kernel_constraint=max_norm(5.)))(Pos2)
 
 # second bi-directional LSTM layer
 
@@ -67,15 +71,29 @@ X = Dropout(0.25)(X)
 
 X = TimeDistributed(Dense(32,kernel_constraint=max_norm(5.)))(X)
 X = TimeDistributed(Dropout(0.25))(X)
-X = TimeDistributed(Dense(numSuperClasses, name='super', activation='softmax',kernel_constraint=max_norm(5.)))(X)
+super_output = TimeDistributed(Dense(numSuperClasses, name='super_output', activation='softmax',kernel_constraint=max_norm(5.)))(X)
 
-model = Model(sentence_embeddings, [Pos1,Pos2,X])
+model = Model(sentence_embeddings, [pos1_output,pos2_output,super_output])
 
 model.summary()
 
 model.compile(loss=['categorical_crossentropy','categorical_crossentropy','categorical_crossentropy'], optimizer='rmsprop', metrics=['accuracy'])
 
+checkpoint = ModelCheckpoint(best_file, monitor='val_time_distributed_9_acc', verbose=1, save_best_only=True, mode='max')
+
+save_current = ModelCheckpoint(current_file, monitor='val_time_distributed_9_acc', verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+
+
+reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2,\
+                                              verbose=1,patience=5, min_lr=0.0001)
+
+log = CSVLogger('elmo_training_log.csv')
+
 
 history = model.fit_generator(training_generator,\
-                              epochs=30, shuffle=True, workers=2, validation_data=validation_generator)
+                              epochs=30, shuffle=True, workers=2, use_multiprocessing=True,\
+                              callbacks = [checkpoint,reduce_lr,log,save_current],
+                              validation_data=validation_generator)
 
+
+model.save('test_superpos.h5')
