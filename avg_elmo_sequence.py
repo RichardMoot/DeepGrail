acb485fb3b2785f8360bd795695ed8ac92f35aa5
Richard Moot <richard.moot@icloud.com>
2021-02-14 19:01:01 +0100

    Scripts for training with ELMo data in TLGbank directory

---
+++ avg_elmo_sequence.py
@@ -0,0 +1,74 @@
+import numpy as np
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, list_IDs, batch_size=32, n_pos1_classes=30, n_pos2_classes=32, n_super_classes=891, shuffle=True):
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.n_pos1_classes = n_pos1_classes
+        self.n_pos2_classes = n_pos2_classes
+        self.n_super_classes = n_super_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.floor(len(self.list_IDs)/self.batch_size))
+
+    def __getitem__(self, index):
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        list_IDs_tmp = [self.list_IDs[k] for k in indexes]
+        
+        embavg, pos1, pos2, super = self.__data_generation(list_IDs_tmp)
+
+        return embavg, [keras.utils.to_categorical(pos1, num_classes=self.n_pos1_classes),\
+                     keras.utils.to_categorical(pos2, num_classes=self.n_pos2_classes),\
+                     keras.utils.to_categorical(super, num_classes=self.n_super_classes)]
+        
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_tmp):
+        em0 = []
+        p1tmp = []
+        p2tmp = []
+        suptmp = []
+        maxlen = 0
+        for i, ID in enumerate(list_IDs_tmp):
+            f = np.load('TLGbank/' + ID + '.npz')
+            p1 = f['pos1']
+            p2 = f['pos2']
+            sup = f['super']
+            e0 = f['embavg']
+            l = len(p1)
+            if l > maxlen:
+                maxlen = l
+            p1tmp.append(p1)
+            p2tmp.append(p2)
+            suptmp.append(sup)
+            em0.append(e0)
+        
+        p1 = np.zeros((self.batch_size, maxlen))
+        for i in range(self.batch_size):
+            for j in range(len(p1tmp[i])):
+                p1[i][j] = p1tmp[i][j]
+
+        p2 = np.zeros((self.batch_size, maxlen))
+        for i in range(self.batch_size):
+            for j in range(len(p2tmp[i])):
+                p2[i][j] = p2tmp[i][j]
+
+        super = np.zeros((self.batch_size, maxlen))
+        for i in range(self.batch_size):
+            for j in range(len(suptmp[i])):
+                super[i][j] = suptmp[i][j]
+                
+        emb0 = np.zeros((self.batch_size, maxlen, 1024))
+        for i in range(self.batch_size):
+            for j in range(len(em0[i])):
+                emb0[i][j] = em0[i][0][j]
+            
+        return emb0, p1, p2, super
+
---
+++ avg_sequence_script_var.py
@@ -0,0 +1,132 @@
+import numpy as np
+import tensorflow as tf
+import os, random
+
+from sklearn.model_selection import train_test_split
+from keras.models import Model, load_model
+from keras.layers import Bidirectional, Lambda, Masking, Dense, Input, Dropout, LSTM, Activation, TimeDistributed, BatchNormalization, concatenate, Concatenate
+from keras.layers.embeddings import Embedding
+from keras.constraints import max_norm, min_max_norm, unit_norm
+from keras import regularizers
+from keras.initializers import random_uniform
+from keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, TensorBoard
+from keras.utils import to_categorical
+from keras import backend as K
+from avg_elmo_sequence import DataGenerator
+
+# variant of avg_sequence_script with extra Dense layer
+# this should give the model the opportunity to better exploit the ELMo
+# vectors
+
+# set random seed to seed_value for reproducability
+
+seed_value = 1
+os.environ['PYTHONHASHSEED'] = str(seed_value)
+random.seed(seed_value)
+np.random.seed(seed_value)
+tf.random.set_seed(seed_value)
+
+# parameters for generator class
+
+params = {'n_pos1_classes':30,
+          'n_pos2_classes':32,
+          'n_super_classes':891,
+          'shuffle':True,
+          'batch_size':32}
+
+# filenames for best and last model files
+
+best_file = 'best_gen_elmo_superpos.h5'
+current_file = 'current_gen_elmo_superpos.h5'
+
+# number of sentences in the treebank; presupposes the existence of file "sent%06d.npz"
+# for i from 0 to treebank_sentences-1 in the TLGbank directory
+
+treebank_sentences = 15748
+
+all = ["sent%06d" %i for i in range(treebank_sentences)]
+
+# standard 60/20/20 split for train/dev/test
+
+train, testdev = train_test_split(all, test_size=0.4)
+test, dev = train_test_split(testdev, test_size=0.5)
+
+print("Train: "+str(len(train)))
+print("Dev  : "+str(len(dev)))
+print("Test : "+str(len(test)))
+
+training_generator = DataGenerator(train, **params)
+validation_generator = DataGenerator(dev, **params)
+
+embLen = 1024
+numPos1Classes = 30
+numPos2Classes = 32
+numSuperClasses = 891
+
+l1_value = 0.0001
+l2_value = 0.0001
+
+# input layers is the (averaged) ELMo output layer
+
+sentence_embeddings = Input(shape = (None,embLen,), dtype = 'float32')
+
+mask = Masking(mask_value=0.0)(sentence_embeddings)
+dropout = Dropout(0.5)(mask)
+#dense = TimeDistributed(Dense(256))(dropout)
+
+# first bi-directional LSTM layer 
+
+X = Bidirectional(LSTM(256, recurrent_dropout=0.2, kernel_constraint=max_norm(4.), return_sequences=True))(dropout)
+X = BatchNormalization()(X)
+X = Dropout(0.25)(X)
+
+# Pos1 output
+
+Pos1 = TimeDistributed(Dense(32,kernel_constraint=max_norm(5.)))(X)
+Pos1 = TimeDistributed(Dropout(0.25))(Pos1)
+pos1_output = TimeDistributed(Dense(numPos1Classes, name='pos1_output', activation='softmax',kernel_constraint=max_norm(4.)))(Pos1)
+
+# Pos2 output
+
+Pos2 = TimeDistributed(Dense(32,kernel_constraint=max_norm(5.)))(X)
+Pos2 = TimeDistributed(Dropout(0.25))(Pos2)
+pos2_output = TimeDistributed(Dense(numPos2Classes, name='pos2_output', activation='softmax',kernel_constraint=max_norm(4.)))(Pos2)
+
+# second bi-directional LSTM layer
+
+X = Bidirectional(LSTM(256, recurrent_dropout=0.25, kernel_constraint=max_norm(4.), return_sequences=True))(X)
+X = BatchNormalization()(X)
+X = Dropout(0.25)(X)
+# concatenate ELMo vectors before output; doesn't improve performance
+# X = concatenate([X,dropout])
+
+# supertag output
+
+X = TimeDistributed(Dense(64,kernel_constraint=max_norm(5.)))(X)
+#X = TimeDistributed(Dense(32,kernel_regularizer=regularizers.l1_l2(l1_value,l2_value)))(X)
+X = TimeDistributed(Dropout(0.25))(X)
+super_output = TimeDistributed(Dense(numSuperClasses, name='super_output', activation='softmax',kernel_regularizer=regularizers.l1_l2(l1_value,l2_value)))(X)
+
+model = Model(sentence_embeddings, [pos1_output,pos2_output,super_output])
+
+model.summary()
+
+model.compile(loss=['categorical_crossentropy','categorical_crossentropy','categorical_crossentropy'], optimizer='rmsprop', metrics=['accuracy'])
+
+checkpoint = ModelCheckpoint(best_file, monitor='val_time_distributed_8_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='max')
+
+save_current = ModelCheckpoint(current_file, monitor='val_time_distributed_8_accuracy', verbose=1, save_best_only=False, save_weights_only=False, mode='auto', save_freq="epoch")
+
+
+reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2,\
+                                              verbose=1,patience=5, min_lr=0.0001)
+
+log = CSVLogger('elmo_training_log.csv')
+
+
+history = model.fit(training_generator,\
+                       epochs=250, shuffle=True, \
+                       callbacks = [checkpoint,reduce_lr,log,save_current],
+                              validation_data=validation_generator)
+
+
